1b59872a9c49dea81dc87339f3b86ae4b5f02381
boedhack99 <boedhack@gmail.com>
2022-12-30 22:34:31 +0700

    ice: Import device configs * Inspired by : https://github.com/yazidkucrit/device_xiaomi_selene
    Change-Id: I2cdba9a55f13209633ce53067893a7a74784fb1a

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_ice.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_ice-user \
+    twrp_ice-userdebug \
+    twrp_ice-eng
