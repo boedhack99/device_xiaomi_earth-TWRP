34e6dfb7d436be1da14830e93ead414cdadae326
boedhack99 <boedhack@gmail.com>
2022-12-31 15:18:29 +0700

    ice: Remove 2ND ARCH flags
    internal error: panic in GenerateBuildActions for module "art-bootclasspath-fragment" variant "android_common_apex31"
    rule "artJarsDexpreopt_android_arm_armv8-a_cortex-a53" is already defined in this scope
    
    Change-Id: Ia57a8ad9fba0dc5f3b86c60dfa36c3a564885573

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,12 +36,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
 TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
