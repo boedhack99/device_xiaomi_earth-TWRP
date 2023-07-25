6af0e0077f1372d28cb813993ae6b21c92143312
boedhack99 <boedhack@gmail.com>
2023-07-25 22:05:56 +0700

    ice: Add back libkeymaster4 to recovery modules
    Change-Id: I47cb6bdb76fbf2335ca4690bc44fe21b1925d49a

--- device.mk
+++ device.mk
@@ -83,10 +83,14 @@ PRODUCT_PACKAGES += \
 
 # Additional configs
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so
 
 TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1
+    android.hardware.keymaster@4.1 \
+    libkeymaster4.so \
+    libkeymaster41.so
 
 # Blacklist
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
